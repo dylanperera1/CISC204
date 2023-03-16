afc6a1d91cc55c5c93684743a3e6c5db99412472
dylanperera1 <dperera@udel.edu>
2023-03-15 22:49:00 -0400

    made the sprites

---
+++ memory game/memory game/Assets/MemoryCard.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour
+{
+    public void OnMouseDown()
+    {
+        Debug.Log("testing 1 2 3");
+    }
+}
